e1c304c77e7c99830c34cd39d5a16e9f94a7b7e7
AmberYounus <65040261+AmberYounus@users.noreply.github.com>
2023-06-01 00:19:51 +0500

    Update App.js
    Remove few lines

--- src/App.js
+++ src/App.js
@@ -7,12 +7,9 @@ function App() {
   const[listOfCoins,setListOfCoins] = useState([]);
   const[searchWord,setSearchWord] = useState("")
   useEffect(()=>{
-   Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
-
-  //  Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then(
+   Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(  
     (response)=>{
       setListOfCoins(response.data.coins)
-      // console.log(response.data);
     }
    )
   },[])
@@ -21,21 +18,20 @@ function App() {
   })
   return (
     <div className="App">
-      <div className="cryptoHeader">
-      <h2>Crypto Price</h2>
+      <div className="cryptoHeader">  
         <input type="text" placeholder='Bitcoin...' onChange={(event)=>{
           setSearchWord(event.target.value)
         }}/>
       </div>
     <div className="cryptoDisplay">
-    <div class="col-sm-3 col-md-9 col-lg-4 col-xl-2">
+ 
 
       {filteredCoins.map((coin)=>{
         return <Coin name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol}/>
       })}
     </div>
     </div>
-    </div>
+   
   
   );
 }
